f23328090f59539bbbee667770a7e405fafcc0e8
Prabhash Swain <pravas.ultracures@gmail.com>
2021-03-25 15:21:02 +0530

    project structure completed

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const PORT = process.env.PORT || 3000
 
 const app = express()
 
+//routes
+app.get('/',(req,res)=>{
+    res.send('Hello Express')
+})
 //server creation
 app.listen(PORT,()=>{
     console.log('Server started');
