699b8c82f6439e571fe66ce0c1a31855a7a708bb
Prabhash Swain <pravas.ultracures@gmail.com>
2021-03-25 15:49:25 +0530

    template engine setup completed

---
+++ controllers/homeController.js
@@ -0,0 +1,7 @@
+const homeController = (req,res)=>{
+    return res.render('home/index')
+}
+
+module.exports = {
+    homeController
+}
--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 const express = require('express');
+const expressLayout = require('express-ejs-layouts');
+const homeRouter = require('./routes/homeRoute');
 require('dotenv').config()
 
 const PORT = process.env.PORT || 3000
 
 const app = express()
 
+//ejs setup
+app.use(expressLayout)
+app.set('view engine','ejs')
 //routes
-app.get('/',(req,res)=>{
-    res.send('Hello Express js')
-})
+app.use(homeRouter)
 //server creation
 app.listen(PORT,()=>{
     console.log('Server started');
